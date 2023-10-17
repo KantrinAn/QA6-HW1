5337ee1f857ad97114eb9f201f967c0b0e19e609
Katrin <anpipinakaka@gmail.com>
2023-10-17 12:51:07 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 7;
+        int b = 9;
+        a = b;
+        b = a-2;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
